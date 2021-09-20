33895104b390e2253960c84653646e93d68cc76a
colbyfayock <fayock@gmail.com>
2021-09-20 11:49:53 -0400

    Step 4: adding Pinned Repositories to the app

--- pages/index.js
+++ pages/index.js
@@ -24,33 +24,14 @@ export default function Home({ pinnedItems }) {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {pinnedItems.map(item => {
+            return (
+              <a key={item.id} href={item.url} className={styles.card}>
+                <h2>{ item.name }</h2>
+                <p>⭐ {item.stargazers.totalCount }</p>
+              </a>
+            )
+          })}
         </div>
       </main>
 
